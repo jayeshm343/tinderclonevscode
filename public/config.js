cef6ab9cf675e74cd26c72e6b9ab3e4f7b62b12b
jayeshm343 <67365285+jayeshm343@users.noreply.github.com>
2023-06-12 20:45:52 +0530

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,6 @@
+const config = {
+    CometChatAppId: 2407909d7988e6a9,
+    CometChatRegion: US,
+    CometChatAuthKey:ad35d82863415609d6596a8b8e47cd596e3356d4,
+    CometChatAPIKey: 1633a0661a933470482af9afd72f60d9e5a8edde
+  };
